f022f8f40bef473e2b758b1bcdaf17a9f7b3be09
Anjali Dange <anjalidange6647@gmail.com>
2026-05-05 23:58:51 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,44 +1,22 @@
-package Interface;
-//interface used to achieve 100% abstraction
-//interface can contains only abstract method till java 8 version
-//interface can contains static ,default,and private method from java 9
-//interface can not contains instance method and instance variable
-//instance can not contains constructor
-//we can not create the object of interface
-//class can not extends interface
-//class can implements interface
-//we override the method of interface into Test class
-public interface Test {
-/*
- * Test(){
-         //no constructor in interface	
-         }
- */
-	//static method
-	static void food() {
-		System.out.println("food");
+package Abstraction;
+//we can access all the members of Student class
+public class Test extends Student{
+@Override
+	void login(String email,int pass) {
+	System.out.println("login successful");
+}
+Test(){
+	System.out.println("this is TEst class constructor");
+}
+	@Override
+	void good() {
+		System.out.println("she is literally good person");
 	}
-	//default method
-	default void good() {
-		System.out.println("good");
+	public static void main(String[] args) {
+		Test t1=new Test();
+		t1.good();
+		t1.login("anjali@gmail.com",121234);
+		
 	}
-	//private method
-	private void m1() {
-		System.out.println("anjali");
-		//no instance variable is there
-	}
-	public static final double salary=100000;
-	int id=4;//public static final by default here we have to give value compulsory
-    String name="anjali";//public static final
-   void login();//this  is abstract method compiler will write
-                 //automatically public abstract before to this
-   abstract void registration();
-    public static void main(String[] args) {
-  //Test t1=new Test();//we can't create object of interface  	
-    System.out.println(Test.name);//because by default its static no
-                              //so we can access it via class name
-   Test.food();
-    Demo.m4();
-    //Test.m4(); not possible by interface
-    }
+	
 }
