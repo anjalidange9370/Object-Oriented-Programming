dd250b65061375c735fd2d190566341ac1c85136
Anjali Dange <anjalidange6647@gmail.com>
2026-05-05 20:27:23 +0530

    Add files via upload

---
+++ VarriablesAccess.java
@@ -0,0 +1,14 @@
+//we cant access instance varible in static method
+//we can access instance variable in instance method
+//we can access static variables in static method
+//we can't access instance variables in static methods
+public class VarriablesAccess {
+    String name="aniket";
+
+
+
+
+
+
+
+}
